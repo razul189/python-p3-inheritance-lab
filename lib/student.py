036da6d22c5856f74c705cdf47cec14309baa8e4
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-12-18 16:19:07 -0500

    lab complete

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,14 @@
 
 from user import User
 
+knowledge = []
+
 class Student(User):
+
+    def __init__(self,first_name,last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge= knowledge
     
-    def learn(self):
-        pass
+    def learn(self, knowledge_detail):
+        self.knowledge.append(knowledge_detail)
+        
